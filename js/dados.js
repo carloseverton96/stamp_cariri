94ddf028aa3e18192df1a346e9a7df4d80eecf45
Éverton Alves <140563844+carloseverton96@users.noreply.github.com>
2024-07-29 08:42:53 -0300

    29/07/2024

--- js/dados.js
+++ js/dados.js
@@ -24,58 +24,8 @@ var MENU = {
             "name": "Caneca personalizada - Branca",
             "price": 20
         },
-        {
-            "id": "caneca-5",
-            "img": "./img/cardapio/canecas/caneca-6.png",
-            "name": "Caneca personalizada - Branca",
-            "price": 20
-        },
-        {
-            "id": "caneca-6",
-            "img": "./img/cardapio/canecas/caneca-6.png",
-            "name": "Caneca personalizada - Branca",
-            "price": 20
-        },
-        {
-            "id": "caneca-7",
-            "img": "./img/cardapio/canecas/caneca-7.png",
-            "name": "Caneca personalizada - Branca",
-            "price": 20
-        },
-        {
-            "id": "caneca-8",
-            "img": "./img/cardapio/canecas/caneca-8.png",
-            "name": "Caneca personalizada - Branca",
-            "price": 20
-        },
-        {
-            "id": "caneca-9",
-            "img": "./img/cardapio/canecas/caneca-9.png",
-            "name": "Caneca personalizada - Branca",
-            "price": 20
-        },
-        {
-            "id": "caneca-10",
-            "img": "./img/cardapio/canecas/caneca-10.png",
-            "name": "Caneca personalizada - Branca",
-            "price": 20
-        },
-        {
-            "id": "caneca-11",
-            "img": "./img/cardapio/canecas/caneca-11.png",
-            "name": "Caneca personalizada - Branca",
-            "price": 20
-        },
-        {
-            "id": "caneca-12",
-            "img": "./img/cardapio/canecas/caneca-12.png",
-            "name": "Caneca personalizada - Branca",
-            "price": 20
-        }
-
-
-    ],    
-    "camisas": [
+        
+// CAMISA
         {
             "id": "camisa-1",
             "img": "./img/cardapio/camisas/camisa-1.png",
@@ -100,57 +50,9 @@ var MENU = {
             "name": "Camisa cinza",
             "price": 20
         },
-        {
-            "id": "camisa-5",
-            "img": "./img/cardapio/camisas/camisa-5.png",
-            "name": "Camisa branca",
-            "price": 18
-        },
-        {
-            "id": "camisa-6",
-            "img": "./img/cardapio/camisas/camisa-6.png",
-            "name": "Camisa branca",
-            "price": 18
-        },
-        {
-            "id": "camisa-7",
-            "img": "./img/cardapio/camisas/camisa-7.png",
-            "name": "Camisa cinza",
-            "price": 20
-        },
-        {
-            "id": "camisa-8",
-            "img": "./img/cardapio/camisas/camisa-8.png",
-            "name": "Camisa cinza",
-            "price": 20
-        },
-        {
-            "id": "camisa-9",
-            "img": "./img/cardapio/camisas/camisa-9.png",
-            "name": "Camisa branca",
-            "price": 18
-        },
-        {
-            "id": "camisa-10",
-            "img": "./img/cardapio/camisas/camisa-10.png",
-            "name": "Camisa branca",
-            "price": 18
-        },
-        {
-            "id": "camisa-11",
-            "img": "./img/cardapio/camisas/camisa-11.png",
-            "name": "Camisa cinza",
-            "price": 20
-        },
-        {
-            "id": "camisa-12",
-            "img": "./img/cardapio/camisas/camisa-12.png",
-            "name": "Camisa cinza",
-            "price": 20
-        }
-        
-    ],
-    "coracao": [
+    
+// CORAÇÃO
+
         {
             "id": "coracao-1",
             "img": "./img/cardapio/coracao/coracao-1.png",
@@ -175,56 +77,7 @@ var MENU = {
             "name": "Caneca alça coração",
             "price": 30
         },
-        {
-            "id": "coracao-1",
-            "img": "./img/cardapio/coracao/coracao-1.png",
-            "name": "Caneca alça coração",
-            "price": 30
-        },
-        {
-            "id": "coracao-2",
-            "img": "./img/cardapio/coracao/coracao-2.png",
-            "name": "Caneca alça coração",
-            "price": 30
-        },
-        {
-            "id": "coracao-3",
-            "img": "./img/cardapio/coracao/coracao-3.png",
-            "name": "Caneca alça coração",
-            "price": 30
-        },
-        {
-            "id": "coracao-4",
-            "img": "./img/cardapio/coracao/coracao-4.png",
-            "name": "Caneca alça coração",
-            "price": 30
-        },
-        {
-            "id": "coracao-1",
-            "img": "./img/cardapio/coracao/coracao-1.png",
-            "name": "Caneca alça coração",
-            "price": 30
-        },
-        {
-            "id": "coracao-2",
-            "img": "./img/cardapio/coracao/coracao-2.png",
-            "name": "Caneca alça coração",
-            "price": 30
-        },
-        {
-            "id": "coracao-3",
-            "img": "./img/cardapio/coracao/coracao-3.png",
-            "name": "Caneca alça coração",
-            "price": 30
-        },
-        {
-            "id": "coracao-4",
-            "img": "./img/cardapio/coracao/coracao-4.png",
-            "name": "Caneca alça coração",
-            "price": 30
-        }
-    ],
-    "azulejos": [
+        // AZULEJOS
         {
             "id": "azulejo-1",
             "img": "./img/cardapio/azulejos/azulejo-1.png",
@@ -249,56 +102,9 @@ var MENU = {
             "name": "Azulejo 20x20 personalizada",
             "price": 15
         },
-        {
-            "id": "azulejo-5",
-            "img": "./img/cardapio/azulejos/azulejo-5.png",
-            "name": "Azulejo 10x10 personalizado",
-            "price": 10
-        },
-        {
-            "id": "azulejo-6",
-            "img": "./img/cardapio/azulejos/azulejo-6.png",
-            "name": "Azulejo 10x10 personalizado",
-            "price": 10
-        },
-        {
-            "id": "azulejo-7",
-            "img": "./img/cardapio/azulejos/azulejo-7.png",
-            "name": "Azulejo 20x20 personalizada",
-            "price": 15
-        },
-        {
-            "id": "azulejo-8",
-            "img": "./img/cardapio/azulejos/azulejo-8.png",
-            "name": "Azulejo 20x20 personalizada",
-            "price": 15
-        },
-        {
-            "id": "azulejo-9",
-            "img": "./img/cardapio/azulejos/azulejo-9.png",
-           "name": "Azulejo 10x10 personalizado",
-            "price": 10
-        },
-        {
-            "id": "azulejo-10",
-            "img": "./img/cardapio/azulejos/azulejo-10.png",
-            "name": "Azulejo 10x10 personalizado",
-            "price": 10
-        },
-        {
-            "id": "azulejo-11",
-            "img": "./img/cardapio/azulejos/azulejo-11.png",
-            "name": "Azulejo 20x20 personalizada",
-            "price": 15
-        },
-        {
-            "id": "azulejo-12",
-            "img": "./img/cardapio/azulejos/azulejo-12.png",
-            "name": "Azulejo 20x20 personalizada",
-            "price": 15
-        }
-    ],
-    "ecobags": [
+
+        // ECOBAG
+
         {
             "id": "ecobag-1",
             "img": "./img/cardapio/ecobags/ecobag-1.png",
@@ -323,104 +129,9 @@ var MENU = {
             "name": "Ecobag personalizada",
             "price": 25
         },
-        {
-            "id": "ecobag-5",
-            "img": "./img/cardapio/ecobags/ecobag-5.png",
-            "name": "Ecobag personalizada",
-            "price": 25
-        },
-        {
-            "id": "ecobag-6",
-            "img": "./img/cardapio/ecobags/ecobag-6.png",
-            "name": "Ecobag personalizada",
-            "price": 25
-        },
-        {
-            "id": "ecobag-7",
-            "img": "./img/cardapio/ecobags/ecobag-7.png",
-            "name": "Ecobag personalizada",
-            "price": 25
-        },
-        {
-            "id": "ecobag-8",
-            "img": "./img/cardapio/ecobags/ecobag-8.png",
-            "name": "Ecobag personalizada",
-            "price": 25
-        },
-        {
-            "id": "ecobag-9",
-            "img": "./img/cardapio/ecobags/ecobag-9.png",
-            "name": "Ecobag personalizada",
-            "price": 25
-        },
-        {
-            "id": "ecobag-10",
-            "img": "./img/cardapio/ecobags/ecobag-10.png",
-            "name": "Ecobag personalizada",
-            "price": 25
-        },
-        {
-            "id": "ecobag-11",
-            "img": "./img/cardapio/ecobags/ecobag-11.png",
-            "name": "Ecobag personalizada",
-            "price": 25
-        },
-        {
-            "id": "ecobag-12",
-            "img": "./img/cardapio/ecobags/ecobag-12.png",
-            "name": "Ecobag personalizada",
-            "price": 25
-        }
-    ],
-    "combos": [
-        {
-            "id": "combo-1",
-            "img": "./img/cardapio/combos/combo-1.png",
-            "name": "Azulejo + caneca",
-            "price": 30
-        },
-        {
-            "id": "combo-2",
-            "img": "./img/cardapio/combos/combo-2.png",
-            "name": "Azulejo + caneca",
-            "price": 30
-        },
-        {
-            "id": "combo-3",
-            "img": "./img/cardapio/combos/combo-3.png",
-            "name": "Azulejo + caneca + camisa",
-            "price": 50
-        },
-        {
-            "id": "combo-4",
-            "img": "./img/cardapio/combos/combo-4.png",
-            "name": "Azulejo + caneca + camisa",
-            "price": 50
-        },
-        {
-            "id": "combo-1",
-            "img": "./img/cardapio/combos/combo-1.png",
-            "name": "Azulejo + caneca",
-            "price": 30
-        },
-        {
-            "id": "combo-2",
-            "img": "./img/cardapio/combos/combo-2.png",
-            "name": "Azulejo + caneca",
-            "price": 30
-        },
-        {
-            "id": "combo-3",
-            "img": "./img/cardapio/combos/combo-3.png",
-            "name": "Azulejo + caneca + camisa",
-            "price": 50
-        },
-        {
-            "id": "combo-4",
-            "img": "./img/cardapio/combos/combo-4.png",
-            "name": "Azulejo + caneca + camisa",
-            "price": 50
-        },
+
+        // COMBOS
+
         {
             "id": "combo-1",
             "img": "./img/cardapio/combos/combo-1.png",
@@ -445,5 +156,6 @@ var MENU = {
             "name": "Azulejo + caneca + camisa",
             "price": 50
         }
+    
     ]
 }
